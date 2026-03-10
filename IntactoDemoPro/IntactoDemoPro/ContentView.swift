27599bf7c8b26620d92d5565feaceec48d94263d
aireddyanu <aireddy.anu@gmail.com>
2026-03-10 16:55:12 +0530

    Feature_changes done

---
+++ IntactoDemoPro/IntactoDemoPro/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  IntactoDemoPro
+//
+//  Created by Personal on 10/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("Home")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
